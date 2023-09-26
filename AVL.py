93ada1d3ad49e7a57958cd2e83ec247ec9d93439
yurimachado1 <yurimachadomi@gmail.com>
2023-09-26 20:59:56 -0300

    Update AVL.py

--- AVL.py
+++ AVL.py
@@ -79,3 +79,111 @@ class AVLTree:
     def delete(self, root, key):
         if root is None:
             return root
+        
+        if key < root.key:
+            root.left = self.delete(root.left, key)
+        elif key > root.key:
+            root.right = self.delete(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+
+            temp = self.find_min(root.right)
+            root.key = temp.key
+            root.right = self.delete(root.right, temp.key)
+
+        self.update_height(root)
+        balance = self.balance(root)
+
+        if balance > 1:
+            if self.balance(root.left) >= 0:
+                return self.rotate_right(root)
+            else:
+                root.left = self.rotate_left(root.left)
+                return self.rotate_right(root)
+
+        if balance < -1:
+            if self.balance(root.right) <= 0:
+                return self.rotate_left(root)
+            else:
+                root.right = self.rotate_right(root.right)
+                return self.rotate_left(root)
+
+        return root
+
+    def find_min(self, root):
+        if root is None:
+            return None
+        while root.left is not None:
+            root = root.left
+        return root
+
+    def search(self, root, key):
+        if root is None:
+            return False
+        if key == root.key:
+            return True
+        elif key < root.key:
+            return self.search(root.left, key)
+        else:
+            return self.search(root.right, key)
+
+    def insert_key(self, key):
+        self.root = self.insert(self.root, key)
+
+    def delete_key(self, key):
+        self.root = self.delete(self.root, key)
+
+    def search_key(self, key):
+        return self.search(self.root, key)
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def height(self, node):
+        if node is None:
+            return 0
+        return node.height
+
+    def balance(self, node):
+        if node is None:
+            return 0
+        return self.height(node.left) - self.height(node.right)
+
+    def update_height(self, node):
+        if node is None:
+            return
+        node.height = max(self.height(node.left), self.height(node.right)) + 1
+
+    def rotate_right(self, y):
+        x = y.left
+        T2 = x.right
+
+        x.right = y
+        y.left = T2
+
+        self.update_height(y)
+        self.update_height(x)
+
+        return x
+
+    def rotate_left(self, x):
+        y = x.right
+        T2 = y.left
+
+        y.left = x
+        x.right = T2
+
+        self.update_height(x)
+        self.update_height(y)
+
+        return y
