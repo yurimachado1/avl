6eb26c6b908ecc03203fcc60b045eb58b462f32f
yurimachado1 <yurimachadomi@gmail.com>
2023-09-26 23:15:37 -0300

    Update AVL.py

--- AVL.py
+++ AVL.py
@@ -187,3 +187,94 @@ class AVLTree:
         self.update_height(y)
 
         return y
+    
+    def insert(self, root, key):
+        if root is None:
+            return Node(key)
+        
+        if key < root.key:
+            root.left = self.insert(root.left, key)
+        else:
+            root.right = self.insert(root.right, key)
+
+        self.update_height(root)
+        balance = self.balance(root)
+
+        if balance > 1:
+            if key < root.left.key:
+                return self.rotate_right(root)
+            else:
+                root.left = self.rotate_left(root.left)
+                return self.rotate_right(root)
+
+        if balance < -1:
+            if key > root.right.key:
+                return self.rotate_left(root)
+            else:
+                root.right = self.rotate_right(root.right)
+                return self.rotate_left(root)
+
+        return root
+
+    def delete(self, root, key):
+        if root is None:
+            return root
+
+        if key < root.key:
+            root.left = self.delete(root.left, key)
+        elif key > root.key:
+            root.right = self.delete(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+
+            temp = self.find_min(root.right)
+            root.key = temp.key
+            root.right = self.delete(root.right, temp.key)
+
+        self.update_height(root)
+        balance = self.balance(root)
+
+        if balance > 1:
+            if self.balance(root.left) >= 0:
+                return self.rotate_right(root)
+            else:
+                root.left = self.rotate_left(root.left)
+                return self.rotate_right(root)
+
+        if balance < -1:
+            if self.balance(root.right) <= 0:
+                return self.rotate_left(root)
+            else:
+                root.right = self.rotate_right(root.right)
+                return self.rotate_left(root)
+
+        return root
+
+    def find_min(self, root):
+        if root is None:
+            return None
+        while root.left is not None:
+            root = root.left
+        return root
+
+    def search(self, root, key):
+        if root is None:
+            return False
+        if key == root.key:
+            return True
+        elif key < root.key:
+            return self.search(root.left, key)
+        else:
+            return self.search(root.right, key)
+
+    def insert_key(self, key):
+        self.root = self.insert(self.root, key)
+
+    def delete_key(self, key):
+        self.root = self.delete(self.root, key)
+
+    def search_key(self, key):
+        return self.search(self.root, key)
